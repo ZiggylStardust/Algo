7ad0a27726658ef1e961b2efb777fe470f47e0c3
Tobias <tobias.fetzer97@googlemail.com>
2018-10-02 14:57:02 +0200

    Ausgabe ohne speichern der wege, pfeil symbole im string

--- src/BruteForcePfeile.java
+++ src/BruteForcePfeile.java
@@ -6,34 +6,28 @@ import static java.lang.System.nanoTime;
 public class BruteForcePfeile {
 
     static int ziel;
-    static int schritte;
     static long anzahlWege;
-    static ArrayList<String> möglichewge;
 
     public static void main(String[] args) {
 
 
+
         System.out.println("Ziel eingeben");
         Scanner scanner = new Scanner(System.in);
-        int anzahl = scanner.nextInt();
+        ziel = scanner.nextInt();
         scanner.close();
-        for (int n = 0; n < anzahl; n++) {
-            möglichewge = new ArrayList<>();
-            ziel = n;
-            schritte = 2 * ziel;
             long anfang = nanoTime();
 
-            laufschritt(0, 0, schritte, "");
+            laufschritt(0, 0, 2*ziel, "");
             long ende = nanoTime();
 
             long dauer = ende - anfang;
 
             System.out.println("ziel: " + ziel + " wege: " + anzahlWege + " dauer: " + dauer / 1e9 + "sec");
             anzahlWege = 0;
-            System.out.println(möglichewge);
             System.out.println();
 
-        }
+
     }
 
     /**
@@ -46,26 +40,24 @@ public class BruteForcePfeile {
      */
     public static void laufschritt(int x, int y, int n, String wege) {
 
-        if (x >= 0 && y >= 0) {                   //Wenn x oder y negativ sind, ist der weg unmöglich.
+        if (x >= 0 && y >= 0&&n>=0) {                   //Wenn x oder y negativ sind, ist der weg unmöglich.
 
 
             if (y == ziel && x == 0 && n == 0) {                        //falls das ziel erreicht wird mit 2n schritten
 
                 anzahlWege++;
-                möglichewge.add(wege);
-
+                System.out.println(wege);
 
             } else if (n >= x && n >= ziel - y) {                       //wenn übrige schritte kleiner als weg nach links oder zum ziel nach oben sind, ist es unmöglich das tziel zu erreichen
 
                 //hoch
-                laufschritt(x, y + 1, n - 1, wege + "hoch ");
+                laufschritt(x, y + 1, n - 1, wege + "↑");
                 //runter
-                laufschritt(x, y - 1, n - 1, wege + "runter ");
+                laufschritt(x, y - 1, n - 1, wege + "↓");
                 //rechts
-                laufschritt(x + 1, y, n - 1, wege + "rechts ");
+                laufschritt(x + 1, y, n - 1, wege + "→");
                 //links hoch
-                laufschritt(x - 1, y + 1, n - 1, wege + "linkshoch ");
-
+                laufschritt(x - 1, y + 1, n - 1, wege + "↖");
             }
 
         }
